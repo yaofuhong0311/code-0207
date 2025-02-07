993341d99b8cf44f28a69d1aecc7779978d95ea4
yaofuhong0311 <751476138@qq.com>
2025-02-07 14:34:00 +0800

    集成环境

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 '''
 用Python实现用户登录过程.
 要求:注意安全的可能性注意权限验证的便捷性，使得其他地方也能使用考虑性能、可扩展性.
-说明如何达到的注意选择的技术栈，说明为什么这样选择请将代码提交到任意GitHub，注意代码规范以及commit规范。上述4点，请在readme文件中进行说明。
 '''
 import time
 import hashlib
+import os
 
 user_info = {
         'user1': hashlib.sha256('password123'.encode()).hexdigest()
